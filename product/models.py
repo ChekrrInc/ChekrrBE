1f8a3c62800c8c095d242b207958c89cbfb75280
Lawrence <lawuche29@gmail.com>
2026-03-15 01:05:53 +0100

    chore:added product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title=models.CharField(max_length=255)
+    image=models.CharField(max_length=255)
+    price=models.CharField(max_length=255)
+    quantity=models.IntegerField()
+    owner_name=models.CharField(max_length=255)
+    store_name=models.CharField(max_length=255)
+    product_hash=models.CharField(max_length=255)
+    is_paid=models.BooleanField()
+
